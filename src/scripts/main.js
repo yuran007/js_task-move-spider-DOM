d20b24078f69b12d48e88b6a47c3f9e51f5a704d
Yuriy Titov <yuranus@gmail.com>
2021-11-17 05:03:29 +0200

    feat: solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.body.querySelector('.wall');
+  const spider = document.body.querySelector('.spider');
+
+  let spiderLeft = e.clientX
+    - wall.offsetLeft
+    - wall.clientLeft
+    - spider.width / 2;
+
+  let spiderTop = e.clientY
+    - wall.offsetTop
+    - wall.clientTop
+    - spider.height / 2;
+
+  if (spiderLeft < 0) {
+    spiderLeft = 0;
+  }
+
+  if (spiderLeft > wall.clientWidth - spider.width) {
+    spiderLeft = wall.clientWidth - spider.width;
+  }
+
+  if (spiderTop < 0) {
+    spiderTop = 0;
+  }
+
+  if (spiderTop > wall.clientHeight - spider.height) {
+    spiderTop = wall.clientHeight - spider.height;
+  }
+
+  spider.style.display = 'block';
+  spider.style.left = `${spiderLeft}px`;
+  spider.style.top = `${spiderTop}px`;
 });
